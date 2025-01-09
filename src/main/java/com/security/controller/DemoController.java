f8b2c74e779f1ceaee4838dc087f32c652b8fd39
ganesh dhore <ganeshdhore757@gmail.com>
2025-01-09 21:41:37 +0530

    first commit

---
+++ src/main/java/com/security/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/")
+	public String demo(HttpServletRequest request) {
+		
+		request.getSession().getId();
+		System.out.println("SESSIONID : "+request.getSession().getId());
+		return "Hello Security ,  We are champion	  -     "+request.getSession().getId();
+	}
+
+}
